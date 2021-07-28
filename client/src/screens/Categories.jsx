b8798b984deef5f2de6522d4f61c6111046f4e95
Kevin Wand <kevinwand@Kevins-Air.home>
2021-07-28 04:13:44 -0400

    categories css

--- client/src/screens/Categories.jsx
+++ client/src/screens/Categories.jsx
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom'
 export default function Categories(props) {
   const { categoryList } = props
   return (
-    <div>
-      <div>Choose a category</div>
-      {categoryList.map((category) => (
-        <div key={category.id}>
-          <Link to={`/categories/${category.id}`}><div>{category.name}</div></Link>
-        </div>
-      ))}
+    <div className='category-wrapper'>
+      <div className='category-header'>Choose a category</div>
+      <div className='category-container'>
+        {categoryList.map((category) => (
+          <div key={category.id} className='category-card'>
+            <Link className='category-link' to={`/categories/${category.id}`}><div className='category-link'>{category.name}</div></Link>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
