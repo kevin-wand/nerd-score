08f3e6c4c283c24d59d4fccc45dd89021cc0246b
Kevin Wand <kevinwand@Kevins-Air.home>
2021-07-28 09:09:45 -0400

    hamburger css

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/OnePage.css'
 
 export default function Layout(props) {
+  const [showNav, setShowNav] = useState(false)
   const { currentUser, handleLogout } = props;
+
+  function toggleNav() {
+    setShowNav(prevState => !prevState);
+    console.log(showNav)
+  }
 
   return (
     <div className='layout-container'>
@@ -11,31 +17,29 @@ export default function Layout(props) {
         <div className='logo-container'>
           <img className="nerd-logo" src='https://i.imgur.com/hSc7opW.jpg' alt='Nerd Score' />
         </div>
-        <div className='dropdown-container'>
-          <div className='user-welcome-container'>
-            {currentUser ? (
-              <div className='user-verification'>
-                <div className='user-signed-in'>Welcome {currentUser.username}!</div>
-                <button onClick={handleLogout} className='user-logout'>Logout</button>
-              </div>
-            ) : (
-              <Link to='/login' className='user-login'>Login</Link>
-            )}
-          </div>
-          <div className='hamburger'>
-            <i className='fas fa-bars fa-2x'></i>
-            <ul>
-              <li><Link to='/'>Home Page</Link></li>
-              <li><Link to='/categories'>Pick a Quiz</Link></li>
-              <li><Link to='/'>Leaderboard</Link></li>
-              <li><Link to='/questions'>Quiz Bank</Link></li>
-              <li><Link to='/login'>Login</Link></li>
-              <li><Link to='/register'>Register</Link></li>
-            </ul>
-          </div>
+        <div className='user-welcome-container'>
+          {currentUser ? (
+            <div className='user-verification'>
+              <div className='user-signed-in'>Welcome {currentUser.username}!</div>
+              <button onClick={handleLogout} className='user-logout'>Logout</button>
+            </div>
+          ) : (
+            <Link to='/login' className='user-login'>Login</Link>
+          )}
+        </div>
+        <i onClick={toggleNav} className={showNav ? 'fas fa-bars fa-2x navbar-true' : 'fas fa-bars fa-2x navbar-false'}></i>
+        <div className={showNav ? 'hamburger-true' : 'hamburger-false'}>
+          <ul>
+            <li><Link to='/'>Home Page</Link></li>
+            <li><Link to='/categories'>Pick a Quiz</Link></li>
+            <li><Link to='/'>Leaderboard</Link></li>
+            <li><Link to='/questions'>Quiz Bank</Link></li>
+            <li><Link to='/login'>Login</Link></li>
+            <li><Link to='/register'>Register</Link></li>
+          </ul>
         </div>
       </header>
       {props.children}
-    </div>
+    </div >
   );
 }
